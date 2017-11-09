9a8e3b4e92fbb789b7b22f073d2a7157df307cbe
Zachary Dudley Friedman <ZacharyDFriedman@gmail.com>
2017-11-09 08:10:22 -0600

    sound only plays when z key is held down

--- public/prox.js
+++ public/prox.js
@@ -16,47 +16,47 @@ ac.suspend()
 ther.frequency.value = tone
 gainNode.gain.value = gain
 
+
+/*  AMBIENT LIGHT CHANGES THE TONE  */
 window.addEventListener('devicelight', function(event){
   let light = event.value
-  let lightMod = Math.pow(1.003, light)
+  // let lightMod = Math.pow(1.003, light)
+  let lightMod = light * 10
   console.log(light, '/', lightMod)
   // ther.frequency.value = event.value * 1000
   ther.frequency.value = light
 })
 
-window.addEventListener('keydown', function(event){
-  // START & STOP | Z
-  if (event.keyCode === 90) {
-    if (ac.state === 'running') {
-      ac.suspend()
-    } else if (ac.state === 'suspended') {
-      ac.resume()
+/*  TONE STARTS WHEN 'Z' IS PRESSED AND CONTINUES UNTIL 'Z' IS PRESSED AGAIN  */
+  // window.addEventListener('keydown', function(event){
+  //   if (event.keyCode === 90) {
+  //     if (ac.state === 'running') {
+  //       ac.suspend()
+  //       console.log('PAUSED')
+  //     } else if (ac.state === 'suspended') {
+  //       ac.resume()
+  //       console.log('PLAYING')
+  //     }
+  //   }
+  // })
+
+
+/*  TONE PLAYS ONLY WHILE 'Z' IS HELD DOWN  */
+  window.addEventListener('keydown', function(event){
+    if (event.keyCode === 90) {
+        ac.resume()
+        console.log('PLAYING')
     }
-  }
+  })
 
-  // // VOLUME UP | UP ARROW
-  // if (event.keyCode === 38) {
-  //   gain += 0.01
-  // }
+  window.addEventListener('keyup', function (event) {
+    if (event.keyCode === 90) {
+      console.log('SUSPEND')
+      ac.suspend()
+    }
+  })
 
-  // // VOLUME DOWN | DOWN ARROW
-  // if (event.keyCode === 40) {
-  //   gain -= 0.01
-  // }
-
-  // // PITCH UP | RIGHT ARROW
-  // if (event.keyCode === 39) {
-  //   tone += 100
-  // }
-
-  // // PITCH DOWN | LEFT ARROW
-  // if (event.keyCode === 37) {
-  //   tone -= 100
-  // }
-
-  // gainNode.gain.value = gain
-  // ther.frequency.value = tone
-})
 
 console.log(gainNode.gain.value)
 console.log(ther.frequency.value)
+
